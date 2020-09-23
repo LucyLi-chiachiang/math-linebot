f00ed3fc7e122b46414a217263c81255e6a00892
donkie <chiachiangli@gmail.com>
2020-09-23 18:46:46 +0800

    [feat] 總之先回應點什麼

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use ReflectionException;
 
 
 class LineWebhookController extends Controller
@@ -35,7 +36,23 @@ class LineWebhookController extends Controller
 
         $events = $request->events;
         foreach ($events as $event) {
-            logger(json_encode($event));
+            // 不是訊息的event先不處理
+            if($event['type'] != 'message') continue;
+            $messageType = $event['message']['type'];
+            $message = $event['message']['text'];
+
+            // 不是文字訊息的類型先不處理
+            if($messageType != 'text') continue;
+
+            try {
+                $response = $bot->replyText($event['replyToken'], $message);
+                if ($response->isSucceeded()) {
+                    logger('reply successfully');
+                    return $this->http200('send success');
+                }
+            } catch (ReflectionException $e) {
+            }
+
         }
         return $this->http200('done');
     }
