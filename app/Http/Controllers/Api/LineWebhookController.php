e60ba075844dc47ba6a99119ed4a107f6a3b5995
donkie <chiachiangli@gmail.com>
2020-09-23 21:33:55 +0800

    [refactor] create CreateLineBot.php Service

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -3,46 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Services\CreateLineBot;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use ReflectionException;
 
 
 class LineWebhookController extends Controller
 {
     //
-    public function webhook (Request $request)
+    public function webhook(Request $request, CreateLineBot $createLineBot)
     {
-        $httpClient = new CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
-        $bot = new LINEBot($httpClient, [
-            'channelSecret' => env('LINE_CHANNEL_SECRET')
-        ]);
-
-        //Signature Validation
-        $signature = $request->header(LINEBot\Constant\HTTPHeader::LINE_SIGNATURE);
-        if(!$signature) {
-            return $this->http403();
-        }
-
-        try {
-            $bot->parseEventRequest($request->getContent(), $signature);
-        } catch (LINEBot\Exception\InvalidSignatureException $exception) {
-            return $this->http403();
-        } catch (LINEBot\Exception\InvalidEventRequestException $exception) {
-            return $this->http403();
-        }
+        $bot = $createLineBot->create($request);
 
         $events = $request->events;
         foreach ($events as $event) {
             // 不是訊息的event先不處理
-            if($event['type'] != 'message') continue;
+            if ($event['type'] != 'message') continue;
             $messageType = $event['message']['type'];
             $message = $event['message']['text'];
 
             // 不是文字訊息的類型先不處理
-            if($messageType != 'text') continue;
+            if ($messageType != 'text') continue;
 
             try {
                 $response = $bot->replyText($event['replyToken'], $message);
@@ -55,11 +37,6 @@ class LineWebhookController extends Controller
 
         }
         return $this->http200('done');
-    }
-
-    private function http403()
-    {
-        abort(403);
     }
 
     private function http200($string)
