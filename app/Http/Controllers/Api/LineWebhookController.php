1af42ce2096cfc3733412db4409a70766713a002
donkie <chiachiangli@gmail.com>
2020-09-23 18:28:03 +0800

    [feat] Signature Validation

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -5,14 +5,48 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 
 class LineWebhookController extends Controller
 {
     //
     public function webhook (Request $request)
     {
-        $params = $request->all();
-        logger(json_encode($params, JSON_UNESCAPED_UNICODE));
-        return response('hello world', 200);
+        $httpClient = new CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
+        $bot = new LINEBot($httpClient, [
+            'channelSecret' => env('LINE_CHANNEL_SECRET')
+        ]);
+
+        //Signature Validation
+        $signature = $request->header(LINEBot\Constant\HTTPHeader::LINE_SIGNATURE);
+        if(!$signature) {
+            return $this->http403();
+        }
+
+        try {
+            $bot->parseEventRequest($request->getContent(), $signature);
+        } catch (LINEBot\Exception\InvalidSignatureException $exception) {
+            return $this->http403();
+        } catch (LINEBot\Exception\InvalidEventRequestException $exception) {
+            return $this->http403();
+        }
+
+        $events = $request->events;
+        foreach ($events as $event) {
+            logger(json_encode($event));
+        }
+        return $this->http200('done');
+    }
+
+    private function http403()
+    {
+        abort(403);
+    }
+
+    private function http200($string)
+    {
+        return response($string, 200);
     }
 }
